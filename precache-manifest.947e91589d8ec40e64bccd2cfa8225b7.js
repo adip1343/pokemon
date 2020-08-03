0f9a9ad48181b3a3db19f47cda5035fb1632e9f8
adip1343 <aadhip43@gmail.com>
2020-08-03 21:48:48 +0530

    Updates

---
+++ precache-manifest.947e91589d8ec40e64bccd2cfa8225b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23692929c2813a589c582d5ea7915f59",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "8388f4cbe48b7b2ee524",
+    "url": "/pokemon/static/css/main.99d947d6.chunk.css"
+  },
+  {
+    "revision": "20b67407b82da9f85892",
+    "url": "/pokemon/static/js/2.06c4d1ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.06c4d1ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8388f4cbe48b7b2ee524",
+    "url": "/pokemon/static/js/main.76125ebd.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
